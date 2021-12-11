2bb20048363b2ac781e2f733542d6789750b4b27
amiraahmed <amira.ahmed@orange.com>
2021-12-11 10:42:34 +0200

    refactor in the steps

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -6,20 +6,20 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
+import org.junit.Assert;
+
 import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 
 
 public class StepDefinitions {
-//    @Title("")
-//    @Test()
-//    @Step
+    @Step
     @Given("The user sets json placeholder API")
     public void the_user_sets_json_placeholder_api() {
         RestAssured.baseURI="https://jsonplaceholder.typicode.com";
 
     }
-//    @Step
+    @Step
     @When("The user gets all user posts by user ID")
     public List the_user_gets_all_user_posts_by_user_id() {
         RestAssured.basePath="/posts/";
@@ -30,20 +30,26 @@ public class StepDefinitions {
         return  post_id;
 
     }
-//    @Step
-    @Then("The user validates all e-mails")
-    public void the_user_validates_all_e_mails() {
+    @Step
+
+    @When("The user gets all comments by post ID")
+    public List the_user_gets_all_comments_by_post_id() {
         RestAssured.basePath = "/comments/";
         List post_id_list = the_user_gets_all_user_posts_by_user_id();
         System.out.println("List of all User Post IDs....."+post_id_list);
         Response response = SerenityRest.given().when().queryParam("id", post_id_list).get();
         List email = response.then().extract().path("email");
-        response.then().assertThat().body("email", is(email));
+        return email;
+    }
+    @Step
+    @Then("The user validates all e-mails")
+    public void the_user_validates_all_e_mails() {
+        List email = the_user_gets_all_comments_by_post_id();
+
          for (int j = 0; j < email.size(); j++)
         {
+            Assert.assertEquals ("email", is(email));
             System.out.println(email.get(j)+ ", Is Valid e-mail ");
-            int statusCode = response.getStatusCode();
-            System.out.println("The status code recieved is : " + statusCode);
 
         }
     }
--- src/test/resources/Features/Validate all user emails.feature
+++ src/test/resources/Features/Validate all user emails.feature
@@ -3,4 +3,5 @@ Feature: Validate user e-mail address
   Scenario: Validate all returned e-mails
     Given The user sets json placeholder API
     When The user gets all user posts by user ID
+    And The user gets all comments by post ID
     Then The user validates all e-mails
